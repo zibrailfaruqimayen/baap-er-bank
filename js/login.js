c855743c2293b036d27d1fa53dcf273ddbcc9ca7
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-12 19:19:40 +0600

    bank page connect to login page

--- js/login.js
+++ js/login.js
@@ -15,9 +15,9 @@ document.getElementById('btn-submit').addEventListener('click', function () {
    // DANGER: DO NOT VERIFY email and password on client side
    // step- 4: verify email and password and check whether valid user or not
    if(email === 'sontan@baap.com' && password === 'secret'){
-      console.log('valid user')
+      window.location.href = 'bank.html'
    }
    else{
-      console.log('invalid user')
+      alert('invalid user')
    }
 })
